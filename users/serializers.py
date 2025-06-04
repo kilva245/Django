0338f14e8491e39bee60e705efa46f514ab37366
kilva245 <139843031+kilva245@users.noreply.github.com>
2025-06-04 13:55:20 +0330

    Create RegisterView for user registration

--- users/serializers.py
+++ users/serializers.py
@@ -3,5 +3,19 @@ from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(Write_only = True, required = True, validators=[validate_password])
-    password2 = serializers.CharField(Write_only = True, required = True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords didn’t match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
